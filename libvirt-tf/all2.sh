82966743494166f8f5b4cebada085ca6953a73ac
Borys Mekh <bmekh@cisco.com>
2023-06-22 19:25:34 +0300

    [RAND-1]: fix sh
    https://jira-eng-rtp3.cisco.com/jira/browse/RAND-1

--- libvirt-tf/all2.sh
+++ libvirt-tf/all2.sh
@@ -31,30 +31,31 @@ func1(){
 	echo "You entered interface: "
 	echo $int
 	curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-apt update -y
-apt install wget make curl unzip net-tools qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils libguestfs-tools genisoimage virtinst libosinfo-bin qemu uml-utilities virt-manager git wget libguestfs-tools p7zip-full gnupg software-properties-common terraform -y
-/usr/bin/make init
-curl -s https://api.github.com/repos/dmacvicar/terraform-provider-libvirt/releases/latest \
-  | grep browser_download_url \
-  | grep linux_amd64.zip \
-  | cut -d '"' -f 4 \
-  | wget -i -
-unzip terraform-provider-libvirt_*_linux_amd64.zip
-rm -f terraform-provider-libvirt_*_linux_amd64.zip
-mkdir -p .terraform.d/plugins/
-mv terraform-provider-libvirt_* .terraform.d/plugins/terraform-provider-libvirt
-echo “security_driver = none” >  /etc/libvirt/qemu.conf 
-systemctl restart libvirtd
-/usr/bin/make apply
-terraform output -json> output_ip.txt
-ip_debian=$(jq -r .ext_ip_debian.value output_ip.txt)
-output_gate_debian= $(/sbin/ifconfig | grep -i 'inet 10.17.3.1' -B1 |head -n 1| awk '{print $1}')
-gate=${output_gate_debian::-1}
-iptables -I FORWARD -o $gate -d  $ip_debian/32 -j ACCEPT
-iptables -t nat -I PREROUTING -p tcp --dport 9867 -j DNAT --to $ip_debian:80
-iptables -A FORWARD -o $gate -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i $gate -o $int -j ACCEPT
-iptables -A FORWARD -i $gate -o lo -j ACCEPT
+  apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" - y
+  apt update -y
+  apt install wget make curl unzip net-tools qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils libguestfs-tools genisoimage virtinst libosinfo-bin qemu uml-utilities virt-manager git wget libguestfs-tools p7zip-full gnupg software-properties-common terraform -y
+  /usr/bin/make init
+  curl -s https://api.github.com/repos/dmacvicar/terraform-provider-libvirt/releases/latest \
+    | grep browser_download_url \
+    | grep linux_amd64.zip \
+    | cut -d '"' -f 4 \
+    | wget -i -
+  unzip terraform-provider-libvirt_*_linux_amd64.zip
+  rm -f terraform-provider-libvirt_*_linux_amd64.zip
+  mkdir -p .terraform.d/plugins/
+  mv terraform-provider-libvirt_* .terraform.d/plugins/terraform-provider-libvirt
+  echo “security_driver = none” >>  /etc/libvirt/qemu.conf 
+  systemctl restart libvirtd
+  /usr/bin/make apply
+  terraform output -json> output_ip.txt
+  ip_debian=$(jq -r .ext_ip_debian.value output_ip.txt)
+  output_gate_debian= $(/sbin/ifconfig | grep -i 'inet 10.17.3.1' -B1 |head -n 1| awk '{print $1}')
+  gate=${output_gate_debian::-1}
+  iptables -I FORWARD -o $gate -d  $ip_debian/32 -j ACCEPT
+  iptables -t nat -I PREROUTING -p tcp --dport 9867 -j DNAT --to $ip_debian:80
+  iptables -A FORWARD -o $gate -m state --state RELATED,ESTABLISHED -j ACCEPT
+  iptables -A FORWARD -i $gate -o $int -j ACCEPT
+  iptables -A FORWARD -i $gate -o lo -j ACCEPT
 }
 func1
 if [ "$?" = "1" ]; then
