d9bebf39935f813360e1296513c585da969f8fdf
root <root@ip-172-31-81-19.ec2.internal>
2023-06-22 09:33:41 +0000

    reorginize network

--- libvirt-tf/network.tf
+++ libvirt-tf/network.tf
@@ -1,8 +1,17 @@
 resource "libvirt_network" "net-internal" {
                     name      = "internal"
-                    mode      = "none"
+                    mode      = "nat"
                     domain    = "internal.local"
                     addresses = ["10.17.4.0/24"]
+                    dhcp {
+                        enabled = true
+                    }
+}
+resource "libvirt_network" "net1" {
+                    name      = "ext"
+                    mode      = "nat"
+                    domain    = "ext.local"
+                    addresses = ["10.17.3.0/24"]
                     dhcp {
                         enabled = true
                     }
